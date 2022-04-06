585628414a2aa255795b90b076828dd6598597e8
jjrv360 <jjrv360@gmail.com>
2022-04-06 12:29:04 -0700

    fixed camera on zoom

--- actions/ZoomCameraAction.java
+++ actions/ZoomCameraAction.java
@@ -25,14 +25,14 @@ public class ZoomCameraAction extends AbstractInputAction
 		if (e.getValue() > -.2 && e.getValue() < .2){
 			return;  // deadzone
 		}else if(e.getComponent().toString().equals("C")){ // Zoom In
-			c = (game.getEngine().getRenderSystem()).getViewport("RIGHT").getCamera();
+			c = (game.getEngine().getRenderSystem()).getViewport("MAIN").getCamera();
 			fwdVector = c.getN();
 			oldPosition = c.getLocation();
 			fwdVector.mul(0.01f);
 			newPosition = oldPosition.add(fwdVector.x(), fwdVector.y(), fwdVector.z());
 			c.setLocation(newPosition);
 		}else if(e.getComponent().toString().equals("V")){ // Zoom Out
-			c = (game.getEngine().getRenderSystem()).getViewport("RIGHT").getCamera();
+			c = (game.getEngine().getRenderSystem()).getViewport("MAIN").getCamera();
 			fwdVector = c.getN();
 			oldPosition = c.getLocation();
 			fwdVector.mul(-0.01f);
